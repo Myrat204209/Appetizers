107c5a6fbabf817c2bcf33e4beaa0ceadd768188
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2026-08-31 12:00:03 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Azat Japarov on 31.08.26.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
