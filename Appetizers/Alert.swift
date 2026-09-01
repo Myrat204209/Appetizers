b0d192825e4a6ee06f3ebfd2d6247b59a014e067
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2026-09-01 12:52:44 +0500

    Alert and error UI addtion

---
+++ Appetizers/Alert.swift
@@ -0,0 +1,8 @@
+//
+//  Alert.swift
+//  Appetizers
+//
+//  Created by Azat Japarov on 31.08.26.
+//
+
+import Foundation
---
+++ Appetizers/LoadingView.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingView.swift
+//  Appetizers
+//
+//  Created by Azat Japarov on 01.09.26.
+//
+
+import Foundation
