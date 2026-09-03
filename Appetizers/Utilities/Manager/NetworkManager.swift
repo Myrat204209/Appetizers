812a6935cec6d49754976997566562e35016a8b5
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2026-09-03 15:07:12 +0500

    Replacing the core logic with async/await API of Swift

--- Appetizers/Utilities/Manager/NetworkManager.swift
+++ Appetizers/Utilities/Manager/NetworkManager.swift
@@ -17,43 +17,59 @@ final class NetworkManager {
     static let baseURL = "https://my-json-server.typicode.com/Myrat204209/Appetizer-fakeserver/"
     private let appetizerURL = baseURL + "request"
     
+
+    //    func getAppetizers(completed : @escaping (Result<[Appetizer], AppetizerError>) -> Void) {
+    //        guard let url = URL(string: appetizerURL) else {
+    //            completed(.failure(.invalidURL))
+    //            return
+    //        }
+    //
+    //        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+    //            guard error == nil else {
+    //                completed(.failure(.unableToComplete))
+    //                return
+    //            }
+    //
+    //            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+    //                completed(.failure(.invalidResponse))
+    //                return
+    //            }
+    //
+    //            guard let data = data else {
+    //                completed(.failure(.invalidData))
+    //                return
+    //            }
+    //            print("Data from the server \(type(of: data))")
+    //            Task { @MainActor in
+    //                do {
+    //                    let decoder = JSONDecoder()
+    ////                    let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
+    //                    let decodedResponse = try decoder.decode([Appetizer].self, from: data)
+    //                    completed(.success(decodedResponse))
+    //                } catch {
+    //                    completed(.failure(.invalidData))
+    //                }
+    //            }
+    //        }
+    //
+    //        task.resume()
+    //    }
     
-    
-    func getAppetizers(completed : @escaping (Result<[Appetizer], AppetizerError>) -> Void) {
+    func getAppetizers()  async throws -> [Appetizer] {
         guard let url = URL(string: appetizerURL) else {
-            completed(.failure(.invalidURL))
-            return
+            throw AppetizerError.invalidURL
         }
         
-        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            guard error == nil else {
-                completed(.failure(.unableToComplete))
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(.failure(.invalidResponse))
-                return
-            }
-            
-            guard let data = data else {
-                completed(.failure(.invalidData))
-                return
-            }
-            print("Data from the server \(type(of: data))")
-            Task { @MainActor in
-                do {
-                    let decoder = JSONDecoder()
-//                    let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
-                    let decodedResponse = try decoder.decode([Appetizer].self, from: data)
-                    completed(.success(decodedResponse))
-                } catch {
-                    completed(.failure(.invalidData))
-                }
-            }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        do {
+            let decoder = JSONDecoder()
+            let decodedResponse = try decoder.decode([Appetizer].self, from: data)
+            return decodedResponse
+        } catch {
+            throw AppetizerError.invalidData
         }
         
-        task.resume()
     }
     
     
